aabb826121d0a36fe61fd0c2ae0df7d7ef3fafaa
bhanu <bhanu@impingeonline.com>
2016-03-23 11:36:37 +0530

    cloudinary update

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,20 +4,20 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  # storage :fog
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
-  uploader = PhotoUploader.new
+  # uploader = PhotoUploader.new
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -35,9 +35,9 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  version :thumb do
-    process :resize_to_fit => [670, 470]
-  end
+  # version :thumb do
+  #   process :resize_to_fit => [670, 470]
+  # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
