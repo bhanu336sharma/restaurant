dad2e1c5bed68655bd4c1f0f9926ee71893a1f11
bhanu <bhanu@impingeonline.com>
2016-04-04 15:04:46 +0530

    omniauth gem for google signup

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   config.omniauth :google_oauth2, "569087001581-jal0k9p6oboe1tr8heaf0bmfgq3q2kvk.apps.googleusercontent.com
-", "sw2npvtc5fb-NGQkylv4U8rB", { access_type: "offline", approval_prompt: "" }
+", "sw2npvtc5fb-NGQkylv4U8rB", { access_type: "online", approval_prompt: "" }
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
