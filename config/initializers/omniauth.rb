d77846eaa9a64b78817d7cfe5390da8fb9faef65
bhanu <bhanu@impingeonline.com>
2016-04-04 15:53:30 +0530

    omniauth gem for google signup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "569087001581-jal0k9p6oboe1tr8heaf0bmfgq3q2kvk.apps.googleusercontent.com","sw2npvtc5fb-NGQkylv4U8rB", { scope: ['email', 'https://www.googleapis.com/auth/gmail.modify'],
+  provider :google_oauth2, "569087001581-jal0k9p6oboe1tr8heaf0bmfgq3q2kvk.apps.googleusercontent.com","sw2npvtc5fb-NGQkylv4U8rB", , skip_jwt: true, { scope: ['email', 'https://www.googleapis.com/auth/gmail.modify'],
     access_type: 'offline'}
 end
