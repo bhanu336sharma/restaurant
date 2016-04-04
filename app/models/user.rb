036cbd2e06931267d55f02006e99f2ff8071f2b5
bhanu <bhanu@impingeonline.com>
2016-04-04 14:20:36 +0530

    omniauth gem for google signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,14 @@ class User < ActiveRecord::Base
   	devise :database_authenticatable, :registerable,
   		 :recoverable, :rememberable, :trackable, :validatable,
   		 :omniauthable, :omniauth_providers => [:google_oauth2]
-  		 
+
   	def self.from_omniauth(access_token)
 	    data = access_token.info
 	    user = User.where(:email => data["email"]).first
 
 	    # Uncomment the section below if you want users to be created if they don't exist
 	    unless user
-	        user = User.create(name: data["name"],
+	        user = User.create(
 	           email: data["email"],
 	           password: Devise.friendly_token[0,20]
 	        )
