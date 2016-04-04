df88985fbdb2dc19e137cb74cd3d57348f8fa806
bhanu <bhanu@impingeonline.com>
2016-04-04 16:09:37 +0530

    omniauth gem

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Product::Application.routes.draw do
   resources :items
   resources :book_tables
 
+  get|post /users/auth/:action/callback, :to => 'sessions#create'
   get '/about' => 'items#about'
   get '/contact' => 'items#contact'
   get '/index' => 'items#index'
