7918cab0d1e48c493c308d8227fa973c2f8f8a45
bhanu <bhanu@impingeonline.com>
2016-04-04 13:36:21 +0530

    omniauth gem for google signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Product::Application.routes.draw do
-    devise_for :users, :controllers => {:registrations => "registrations"}
-    devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+    devise_for :users, :controllers => {:registrations => "registrations"}, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
