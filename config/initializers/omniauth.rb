536eeadf9c2b5189d4025b5b1a0bca979924be0f
bhanu <bhanu@impingeonline.com>
2016-04-04 15:57:58 +0530

    omniauth gem for google signup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "569087001581-jal0k9p6oboe1tr8heaf0bmfgq3q2kvk.apps.googleusercontent.com","sw2npvtc5fb-NGQkylv4U8rB", { scope: ['email', 'https://www.googleapis.com/auth/gmail.modify'],
-    access_type: 'offline'}
+  provider :google_oauth2, "569087001581-jal0k9p6oboe1tr8heaf0bmfgq3q2kvk.apps.googleusercontent.com","sw2npvtc5fb-NGQkylv4U8rB", { scope: ['email', 'https://www.googleapis.com/auth/gmail.modify'], access_type: 'offline', skip_jwt: true}
 end
