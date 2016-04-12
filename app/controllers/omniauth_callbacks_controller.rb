865f60a96813d16b56339a274e77830412a7c41e
bhanu <bhanu@impingeonline.com>
2016-04-12 12:15:01 +0530

    .

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -15,10 +15,10 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Linkedin"
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "LinkedIn"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.linked_in_data"] = request.env["omniauth.auth"]
+      session["devise.linkedin_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
