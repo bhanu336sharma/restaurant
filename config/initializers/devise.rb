85c4264e66f5a61200e431212457b8a8df4e6209
bhanu <bhanu@impingeonline.com>
2016-04-12 15:28:46 +0530

    a

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   config.omniauth :google_oauth2, "569087001581-jal0k9p6oboe1tr8heaf0bmfgq3q2kvk.apps.googleusercontent.com", "sw2npvtc5fb-NGQkylv4U8rB", { access_type: "offline", approval_prompt: "" }
-config.omniauth :linkedin, "75jaxgu61c3yw2", "ljLuUBMwJ3tPPNyg"
+config.omniauth :linkedin, "75jaxgu61c3yw2", "ljLuUBMwJ3tPPNyg", :scope => 'r_fullprofile r_emailaddress r_network'
   # LinkedIn.configure do |config|
   #   config.client_id     = "75jaxgu61c3yw2"
   #   config.client_secret = "ljLuUBMwJ3tPPNyg"
