e1e2bdf17359ab957a04bcb64837932a8c0be662
bhanu <bhanu@impingeonline.com>
2016-04-12 12:09:26 +0530

    .

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -18,7 +18,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Linkedin"
       sign_in_and_redirect @user, :event => :authentication
     else
-      session["devise.linkedin_data"] = request.env["omniauth.auth"]
+      session["devise.linked_in_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
