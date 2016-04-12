e0a98790a3e9e2f8de028bf7a777f3141c1d6459
bhanu <bhanu@impingeonline.com>
2016-04-12 15:20:58 +0530

    a

--- Gemfile
+++ Gemfile
@@ -25,8 +25,7 @@ gem 'social-share-button'
 gem 'omniauth-google-oauth2'
 gem 'rails4-autocomplete'
 gem 'jquery-ui-rails'
-gem 'omniauth-linkedin-oauth2'
-
+gem 'omniauth-linkedin'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.2'
 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,12 +14,12 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
   config.omniauth :google_oauth2, "569087001581-jal0k9p6oboe1tr8heaf0bmfgq3q2kvk.apps.googleusercontent.com", "sw2npvtc5fb-NGQkylv4U8rB", { access_type: "offline", approval_prompt: "" }
-
-  LinkedIn.configure do |config|
-    config.client_id     = "75jaxgu61c3yw2"
-    config.client_secret = "ljLuUBMwJ3tPPNyg"
-    config.redirect_uri  = "http://restaurant-product.herokuapp.com/"
-  end
+config.omniauth :linkedin, "75jaxgu61c3yw2", "ljLuUBMwJ3tPPNyg"
+  # LinkedIn.configure do |config|
+  #   config.client_id     = "75jaxgu61c3yw2"
+  #   config.client_secret = "ljLuUBMwJ3tPPNyg"
+  #   config.redirect_uri  = "http://restaurant-product.herokuapp.com/"
+  # end
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
